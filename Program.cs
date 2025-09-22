43d68db94b80d12ddaaa3d9e58a2e0fa9630a6d6
Мусин Михаил <musin.mihail@gmail.com>
2025-09-22 23:21:15 +0300

    CORS

--- Program.cs
+++ Program.cs
@@ -1,7 +1,21 @@
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddOpenApi();
+var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: MyAllowSpecificOrigins,
+                      policy =>
+                      {
+                          // Укажите здесь URL вашего фронтенда на Vercel
+                          policy.WithOrigins("https://angular-portfolio-pi-nine.vercel.app/")
+                                .AllowAnyHeader()
+                                .AllowAnyMethod();
+                      });
+});
 var app = builder.Build();
+app.UseCors(MyAllowSpecificOrigins);
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
