780191f7b813800f869e6706bc3f1d7dbab6d252
Мусин Михаил <musin.mihail@gmail.com>
2025-09-25 09:30:53 +0300

    Удаление комментариев

--- Program.cs
+++ Program.cs
@@ -1,60 +1,38 @@
 using DotNet_portfolio.Data;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.OpenApi.Models; // Добавлено для Swagger
-
+using Microsoft.OpenApi.Models;
 var builder = WebApplication.CreateBuilder(args);
-
-// 1. Add CORS policy
 var myAllowSpecificOrigins = "_myAllowSpecificOrigins";
 builder.Services.AddCors(options =>
 {
     options.AddPolicy(name: myAllowSpecificOrigins, policy =>
     {
-        // Add both your deployed URL and the local Angular development server URL
         policy.WithOrigins("https://angular-portfolio-pi-nine.vercel.app", "http://localhost:4200")
               .AllowAnyHeader()
               .AllowAnyMethod();
     });
 });
-
-// 2. Configure the DbContext with PostgreSQL
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 builder.Services.AddDbContext<PortfolioDbContext>(options =>
     options.UseNpgsql(connectionString));
-
-// Add services to the container.
 builder.Services.AddControllers();
-
-// 3. Add Swagger services
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(c =>
 {
     c.SwaggerDoc("v1", new OpenApiInfo { Title = "Portfolio API", Version = "v1" });
 });
-
-
 var app = builder.Build();
-
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    // 4. Use Swagger and SwaggerUI
     app.UseSwagger();
     app.UseSwaggerUI(c =>
     {
         c.SwaggerEndpoint("/swagger/v1/swagger.json", "Portfolio API V1");
-        c.RoutePrefix = string.Empty; // To serve Swagger UI at application root
+        c.RoutePrefix = string.Empty;
     });
 }
-
-// 5. Use CORS
 app.UseCors(myAllowSpecificOrigins);
-
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
-
 app.Run();
-
